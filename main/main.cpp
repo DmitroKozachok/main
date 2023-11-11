ddd9b1f469b6d5fe1c5c247f6839c6c776826a54
Petruk <PC@DESKTOP-DSO691H>
2023-11-11 17:50:50 +0200

    Підключив SFML

--- main/main.cpp
+++ main/main.cpp
@@ -1,20 +1,24 @@
-#include <iostream>
-
-using namespace std;
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    int x = 9;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    cout << "Hello World!\n";
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-    int d = 32;
-    int a = 21;
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
-    
-    cout << "OK" << endl;
-    //
-    // test
-    //
     return 0;
 }
